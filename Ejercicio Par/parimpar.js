05de074096a22e8071a6ea4be699902cd8d54f34
Lucas <lucasmarinangeli7@gmail.com>
2024-05-03 05:06:09 -0300

    par-impar ejercicio

---
+++ Ejercicio Par/parimpar.js
@@ -0,0 +1,13 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var readlineSync = require("readline-sync");
+var numero = readlineSync.questionInt("Ingrese el numero: ");
+if (numero === 0) {
+    console.log("El numero es cero.");
+}
+else if (numero % 2 === 0) {
+    console.log("El numero es par.");
+}
+else {
+    console.log("El numero es impar.");
+}
